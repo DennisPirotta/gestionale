b7dec3b92e3cc9ae0e333e5cfb6b4b0431bc6f96
Dennis <dennispirotta@gmail.com>
2023-02-01 15:58:30 +0100

    fixing vite build for production

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
-import {defineConfig} from 'vite';
+import {defineConfig,loadEnv} from 'vite';
 import laravel, {refreshPaths} from 'laravel-vite-plugin';
 import path from "path";
 import fs from 'fs';
 
 const host = 'gestionale.dev'
+const env = loadEnv('production',process.cwd(),'')
+const serverConfig = env.APP_ENV === 'production' ? {} : {
+    host,
+    hmr: { host },
+    https: {
+        key: fs.readFileSync('C:\\laragon\\etc\\ssl\\laragon.key'),
+        cert: fs.readFileSync('C:\\laragon\\etc\\ssl\\laragon.crt')
+    }
+}
 
 export default defineConfig({
     resolve: {
@@ -12,14 +21,7 @@ export default defineConfig({
             '@': '/resources/js',
         }
     },
-    server: {
-        host,
-        hmr: { host },
-        https: {
-            key: fs.readFileSync('C:\\laragon\\etc\\ssl\\laragon.key'),
-            cert: fs.readFileSync('C:\\laragon\\etc\\ssl\\laragon.crt')
-        }
-    },
+    server: serverConfig,
     plugins: [
         laravel({
             input: [
